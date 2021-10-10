23e5b51cdf658392a6103e1887862f534e1ce3b6
Vlad Zyshchuk <vlad_zyshchuk@mail.ru>
2021-10-11 02:52:28 +0300

    desktop no end other too

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,5 @@
-console.log('Start Project');
+document.querySelectorAll('.accordion-button').forEach(item=>{
+    item.addEventListener('click', function(){
+        this.classList.toggle('accordion-button--active')
+    })
+})
