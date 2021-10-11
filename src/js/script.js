cd15b96d909c740362a14edfc5a50e1d59b5690d
Vlad Zyshchuk <vlad_zyshchuk@mail.ru>
2021-10-11 16:25:47 +0300

    burger styling, adaptive can

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,22 @@
 document.querySelectorAll('.accordion-button').forEach(item=>{
     item.addEventListener('click', function(){
-        this.classList.toggle('accordion-button--active')
-    })
+        this.classList.toggle('accordion-button--active');
+    });
+});
+
+const burger = document.querySelector('.burger');
+const burgerMenu = document.querySelector('.burger__menu-list')
+let menuOpen = false;
+burger.addEventListener('click', (e)=>{
+    e.preventDefault();
+    if(!menuOpen){
+        burger.classList.add('burger--active');
+        burgerMenu.classList.add('burger__menu--active');
+        menuOpen = true;
+    }
+    else{
+        burger.classList.remove('burger--active');
+        burgerMenu.classList.remove('burger__menu--active');
+        menuOpen = false;
+    }
 })
