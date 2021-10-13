2d5b9098bf188a40352d18596216f95e8fcda62b
Vlad Zyshchuk <vlad_zyshchuk@mail.ru>
2021-10-13 16:50:44 +0300

    modal is ready and main-care and us-team block in adaptive

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,11 @@
+//accordion
 document.querySelectorAll('.accordion-button').forEach(item=>{
     item.addEventListener('click', function(){
         this.classList.toggle('accordion-button--active');
     });
 });
 
+//burger-menu
 const burger = document.querySelector('.burger');
 const burgerMenu = document.querySelector('.burger__menu-list')
 let menuOpen = false;
@@ -20,3 +22,57 @@ burger.addEventListener('click', (e)=>{
         menuOpen = false;
     }
 })
+
+//modal window
+const closePopupBtn = document.querySelector('#close-popup');
+const openPopupBtn = document.querySelector('#ask-btn');
+const popupCall = document.querySelector('#call');
+
+openPopupBtn.addEventListener('click', onOpenPopupBtnClick);
+
+function initCallPopupListeners(){
+    closePopupBtn.addEventListener('click',onClosePopupBtnClick);
+    document.addEventListener('keydown', onCallPopupEscKeydown);
+    popupCall.addEventListener('click', onCallPopupClick);
+}
+
+function removeCallPopupListeners(){
+    closePopupBtn.removeEventListener('click',onClosePopupBtnClick);
+    document.removeEventListener('keydown', onCallPopupEscKeydown);
+    popupCall.removeEventListener('click', onCallPopupClick);
+}
+
+function onOpenPopupBtnClick(e){
+    e.preventDefault();
+    openPopup(popupCall);
+    initCallPopupListeners();
+}
+
+function onClosePopupBtnClick(e){
+    e.preventDefault();
+    closePopup(popupCall);
+    removeCallPopupListeners();
+}
+
+function openPopup(popupActive){
+    popupActive.classList.add('popup--active');  
+}
+
+function closePopup(popupActive){
+    popupActive.classList.remove('popup--active');  
+}
+
+function onCallPopupEscKeydown(e) {
+    if (e.key === "Escape") {
+      e.preventDefault();
+      closePopup(popupCall);
+      removeCallPopupListeners();
+    }
+  }
+  
+  function onCallPopupClick(e) {
+    if (e.target.classList.contains('popup')) {
+        closePopup(popupCall);
+        removeCallPopupListeners();
+    }
+  }
